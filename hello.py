09ccf486485c01e128fe651a1a2b163a38e5e618
VARSHIKHA N R <85569635+Varshikha182@users.noreply.github.com>
2023-04-26 12:56:46 +0530

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,57 @@
+#Importing Libraries 
+
+import pandas as pd
+import numpy as np
+import re          #regular expression
+import seaborn as sns
+import matplotlib.pyplot as plt
+import warnings
+warnings.simplefilter("ignore")
+
+# Read data
+df = pd.read_csv("Language Detection.csv")
+
+# feature and label extraction
+X = df["Text"]
+y = df["Language"]
+
+# Label encoding
+from sklearn.preprocessing import LabelEncoder
+le = LabelEncoder()
+y = le.fit_transform(y)
+
+# cleaning the data
+text_list = []
+
+# iterating through all the text
+for text in X:         
+    text = re.sub(r'[!@#$(),n"%^*?:;~`0-9]', ' ', text) # removes all the symbols and numbers
+    text = re.sub(r'[[]]', ' ', text)   
+    text = text.lower()          # converts all the text to lower case
+    text_list.append(text)       # appends the text to the text_list
+    
+    
+# Encode the feature(text)
+
+from sklearn.feature_extraction.text import CountVectorizer
+cv = CountVectorizer() 
+X = cv.fit_transform(text_list).toarray() # tokenize a collection of text documents and store it
+                                            #in an array
+                                            
+# split the dataset
+from sklearn.model_selection import train_test_split
+x_train, x_test, y_train, y_test = train_test_split(X, y, test_size = 0.20, random_state=32)
+
+# Model Training
+from sklearn.naive_bayes import MultinomialNB  
+model = MultinomialNB()
+model.fit(x_train, y_train)
+
+# prediction
+y_pred = model.predict(x_test)
+
+# model evaluation
+from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
+accuracy = accuracy_score(y_test, y_pred)
+cm = confusion_matrix(y_test, y_pred)
+print("Accuracy is :",accuracy)
